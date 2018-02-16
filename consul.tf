36869952338ca7a2da8e7021fc968ba800dbb7c1
EC2 Default User <ec2-user@ip-172-31-17-77.eu-west-2.compute.internal>
2018-02-16 15:24:01 +0000

    First commit of consul module

---
+++ consul.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    region = "eu-west-2"
+    bucket = "examplecomskg-remote-state-consul"
+    key    = "terraform.tfstate"
+  }
+}
+
+module "remote_state" {
+  source      = "github.com/turnbullpress/tf_remote_state.git"
+  prefix      = "${var.prefix}"
+  environment = "${var.environment}"
+}
+
+module "vpc" {
+  source        = "github.com/turnbullpress/tf_vpc.git?ref=v0.0.1"
+  name          = "consul"
+  cidr          = "${var.vpc_cidr}"
+  public_subnet = "${var.public_subnet}"
+}
+
+module "consul" {
+  source           = "github.com/turnbullpress/tf_consul.git"
+  environment      = "${var.environment}"
+  token            = "${var.token}"
+  encryption_key   = "${var.encryption_key}"
+  vpc_id           = "${module.vpc.vpc_id}"
+  public_subnet_id = "${module.vpc.public_subnet_id}"
+  region           = "${var.region}"
+  key_name         = "${var.key_name}"
+  private_key_path = "${var.private_key_path}"
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "prefix" {
+  description = "The name of our org, i.e. examplecom."
+  default     = "examplecomskg"
+}
+
+variable "environment" {
+  description = "The name of our environment, i.e. development."
+  default     = "consul"
+}
+
+variable "key_name" {
+  description = "The AWS key pair to use for resources."
+}
+
+variable "private_key_path" {
+  description = "The path to the AWS key pair to use for resources."
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR of the VPC."
+}
+
+variable "public_subnet" {
+  description = "The public subnet to populate."
+}
+
+variable "token" {
+  description = "Consul server token"
+}
+
+variable "encryption_key" {
+  description = "Consul encryption key"
+}
